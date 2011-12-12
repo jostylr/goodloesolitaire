9f0d5007afa0a4b60330a1fbb530649cd488825f
James Taylor <jostylr@gmail.com>
2011-12-12 18:19:04 -0500

    starting conversion to node style

--- app.js
+++ app.js
@@ -28,6 +28,8 @@ next();
 app.configure( function () {
 	app.use(setHeaders);
 	app.use(express.bodyParser());
+	var bundle = require('browserify')(__dirname + '/entry.js');
+	app.use(bundle);
 	app.use(express.static(__dirname + '/public'));	
 });
 
