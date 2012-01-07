5d6946e4fe99783f828ff7743bfa25a52094d7e1
James Taylor <jostylr@gmail.com>
2012-01-06 19:06:08 -0500

    tabs replaced with spaces

--- browserify/tests/history_l.js
+++ browserify/tests/history_l.js
@@ -8,33 +8,33 @@ var ui = new ev.EventEmitter();
 require('../logic/history.js')(gcd, ui);
 
 exports.history = function(test) {
-	test.expect(2);
-	ui.on('add history', function (info) {
-		test.equal("hi", "hi");
-		test.deepEqual(info, { num: 1,
-		  score: 230,
-		  deltalablel: undefined,
-		  hand: 
-		   [ 'old',
-		     [ 'T', '&#x2666;' ],
-		     'old',
-		     [ 'K', '&#x2666;' ],
-		     'old',
-		     [ '2', '&#x2663;' ],
-		     'old',
-		     [ '9', '&#x2666;' ],
-		     'old',
-		     [ 'J', '&#x2666;' ] ],
-		  call: 'SF' }, 'add history emitted');
-		test.done(); 
-	});
-	
-	gcd.emit('clear history');
-	gcd.emit('prep for add history', {
-		score: 230, 
-		deltalablel: "t",
-		hand: ["Td", "Kd", "2c", "9d", "Jd"], 
-		call: ["sf", "K"]
-	});
+  test.expect(2);
+  ui.on('add history', function (info) {
+    test.equal("hi", "hi");
+    test.deepEqual(info, { num: 1,
+      score: 230,
+      deltalablel: undefined,
+      hand: 
+       [ 'old',
+         [ 'T', '&#x2666;' ],
+         'old',
+         [ 'K', '&#x2666;' ],
+         'old',
+         [ '2', '&#x2663;' ],
+         'old',
+         [ '9', '&#x2666;' ],
+         'old',
+         [ 'J', '&#x2666;' ] ],
+      call: 'SF' }, 'add history emitted');
+    test.done(); 
+  });
+  
+  gcd.emit('clear history');
+  gcd.emit('prep for add history', {
+    score: 230, 
+    deltalablel: "t",
+    hand: ["Td", "Kd", "2c", "9d", "Jd"], 
+    call: ["sf", "K"]
+  });
 };
 
